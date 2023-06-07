0bdeb1782ca5ff13589e26c1be053d6219845d93
Glenn <glennwiz@gmail.com>
2023-06-07 19:18:03 +0200

    Base2 and Base10

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+class BinaryCounter
+{
+    static void Main()
+    {
+        int countTo = 16; // Change this value to set the desired count limit
+
+        for (int i = 0; i <= countTo; i++)
+        {
+            string binary = Convert.ToString(i, 2);
+            Console.WriteLine($"base2  | {i} | {binary}");
+            Console.WriteLine($"base10 | {i} | {i}");
+
+
+            Thread.Sleep(500); 
+        }
+    }
+}
+
